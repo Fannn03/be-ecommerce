158dd83a376fe5b6d909cdd96ebe306f7015eced
Fannn03 <fann.fann771@gmail.com>
2023-10-13 12:43:27 +0700

    feat: verif email user

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import registerService from "../services/user/register-service";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import loginService from "../services/user/login-service";
-import verifyEmailService from "../services/user/verify-email-service";
+import verifyEmailService, { VerifyEmailError } from "../services/user/verify-email-service";
 
 export const registerUser = async (req: Request, res: Response) => {
   try {
@@ -53,14 +53,26 @@ export const loginUser = async (req: Request, res: Response) => {
 export const verifyEmail = async (req: Request, res: Response) => {
   try {
     await verifyEmailService(req.query)
-    res.send('ok')
   } catch (err) {
-    if(err instanceof Error) {
-      return res.status(400).json({
-        code: 400,
-        result: 'error',
+    if(err instanceof VerifyEmailError) {
+      return res.status(err.code).json({
+        code: err.code,
+        result: err.result,
         message: err.message
-      })
+      });
+    }else {
+      const error: Error = err as Error;
+      return res.status(500).json({
+        code: 500,
+        result: 'internal server error',
+        message: error.message
+      });
     }
   }
+
+  return res.json({
+    code: 200,
+    result: 'success',
+    message: 'success verif email'
+  });
 }
