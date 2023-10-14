6331d68a139009d4636fe1b35a3b773063a806b6
Fannn03 <fann.fann771@gmail.com>
2023-10-14 22:13:49 +0700

    feat: create store route

---
+++ src/controllers/store-controller.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express";
+
+export const createStore = async (req: Request, res: Response) => {
+  
+}
--- src/routers/api-router.ts
+++ src/routers/api-router.ts
@@ -2,10 +2,13 @@ import express from 'express'
 import swaggerUi from 'swagger-ui-express'
 import swaggerDoc from '../docs/api.json'
 import userRouter from './user-router'
+import storeRouter from './store-router'
+import authMiddleware from '../middleware/auth-middleware'
 
 const router = express.Router()
 
 router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 router.use('/users', userRouter)
+router.use('/store', authMiddleware, storeRouter)
 
 export default router
