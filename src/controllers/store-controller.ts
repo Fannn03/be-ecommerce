3dbe75860c98c1f78eb6a028d6ae13c00febf4fd
Fannn03 <fann.fann771@gmail.com>
2023-10-15 10:58:57 +0700

    feat: add create data store services

--- src/controllers/store-controller.ts
+++ src/controllers/store-controller.ts
@@ -1,5 +1,29 @@
 import { Request, Response } from "express";
+import createService, { CreateStoreError } from "../services/store/create-service";
 
 export const createStore = async (req: Request, res: Response) => {
-  
+  try {
+    await createService(req.body, req.user)
+  } catch (err) {
+    if(err instanceof CreateStoreError) {
+      return res.status(err.code).json({
+        code: err.code,
+        result: err.result,
+        message: err.message
+      })
+    } else {
+      const error: Error = err as Error
+      return res.status(500).json({
+        code: 500,
+        result: 'internal server error',
+        message: error.message
+      })
+    }
+  }
+
+  return res.json({
+    code: 200,
+    result: 'success',
+    message: 'success created data store'
+  })
 }
