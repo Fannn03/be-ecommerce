f5bbe291991a95bd3de2f985579650e78910368d
Fannn03 <fann.fann771@gmail.com>
2023-10-18 11:54:06 +0700

    feat: add seeder for superadmin

---
+++ prisma/seeders/super-admin-seeder.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client"
+import bcrypt from 'bcrypt'
+
+const prisma = new PrismaClient()
+
+export default async () => {
+  try {
+    console.log('seeding superadmin...');
+    await prisma.admin.create({
+      data: {
+        email: 'superadmin@gmail.com',
+        name: 'superadmin',
+        password: await bcrypt.hash('password', 10),
+        email_verified: new Date().toISOString(),
+        level: 'superadmin'
+      }
+    })
+    console.log('success');
+  } catch (err) {
+    throw err
+  }
+}
