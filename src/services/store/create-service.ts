db52ca1b3bfd381550e5308e31709858cd5c3f84
Fannn03 <fann.fann771@gmail.com>
2023-10-28 08:38:38 +0700

    fix: remove image when validation error

--- src/services/store/create-service.ts
+++ src/services/store/create-service.ts
@@ -1,4 +1,3 @@
-import { JwtPayload } from "jsonwebtoken"
 import { Request } from "express"
 import fs from 'fs/promises'
 import { createStore } from "../../repositories/store"
@@ -21,22 +20,28 @@ export class CreateStoreError extends Error {
   }
 }
 
-export default async (request: Request, user: JwtPayload) => {
-  request.body.user_id = user.id
-  
+export default async (request: Request) => {
+  request.body.user_id = request.user.id
+
   if(request.file) {
     delete request.body.file
 
     const extension = request.file?.mimetype.split("/")[1]
-    request.body.photos = `${user.id}.${extension}`
-
-    fs.rename(`public/images/temp/${request.file?.filename}`, `public/images/stores/${request.body.photos}`)
+    request.body.photos = `${request.user.id}.${extension}`
   }
 
   try {
     await createStore(request.body)
+
+    if(request.file) {
+      // move file into public store images
+      fs.rename(`public/images/temp/${request.file?.filename}`, `public/images/stores/${request.body.photos}`)
+    }
   } catch (err) {
     if(err instanceof PrismaClientKnownRequestError) {
+      // delete current file in public store images
+      if(request.file) fs.unlink(`public/images/temp/${request.file?.filename}`)
+
       if(err.code === "P2002" && err.meta?.target === "stores_user_id_key") {
         throw new CreateStoreError("This account already has store exists", 400, "bad request")
       } else if (err.code === "P2002" && err.meta?.target === "stores_username_key") {
