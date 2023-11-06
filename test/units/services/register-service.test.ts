95b647f873bbc888a8ab0ca5b9d0adf12e9d4641
Fannn03 <fann.fann771@gmail.com>
2023-11-06 13:13:18 +0700

    test(register service): expect duplicate value

--- test/units/services/register-service.test.ts
+++ test/units/services/register-service.test.ts
@@ -2,12 +2,18 @@ import { faker } from '@faker-js/faker'
 import register from '../../../src/services/user/register-service'
 
 describe("User Register Service", () => {
-  test("Should return data when register success", async () => {
+  let email:string, name:string
+
+  test("Should return property when register success", async () => {
     const payload = {
       email: faker.internet.email(),
       name: faker.internet.userName(),
       password: "password"
     }
+
+    // assign variable email to validate duplicate data
+    email = payload.email
+    name = payload.name
 
     const response = await register(payload)
 
@@ -15,5 +21,33 @@ describe("User Register Service", () => {
     expect(response).toHaveProperty("name")
     expect(response).toHaveProperty("email")
     expect(response).toHaveProperty("createdAt")
+  })
+
+  test("Should return error message when duplicate value email", async () => {
+    const payload = {
+      email: email,
+      name: faker.internet.userName(),
+      password: "password"
+    }
+
+    try {
+      await register(payload)
+    } catch (err: any) {
+      expect(err.message).toBe("Email already taken")
+    }
+  })
+
+  test("Should return error message when duplicate value name", async () => {
+    const payload = {
+      email: faker.internet.email(),
+      name: name,
+      password: "password"
+    }
+
+    try {
+      await register(payload)
+    } catch (err: any) {
+      expect(err.message).toBe("Name already taken")
+    }
   })
 })
