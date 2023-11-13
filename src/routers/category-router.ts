0d963164efd03007bee78f4d2ed76650e568c2a5
Fannn03 <fann.fann771@gmail.com>
2023-11-13 15:55:42 +0700

    feat (create): create category middleware

--- src/routers/category-router.ts
+++ src/routers/category-router.ts
@@ -1,9 +1,10 @@
 import express, { Router } from 'express'
 import { createCategory } from '../controllers/category-controller';
 import { allowedLevels } from '../middleware/auth-middleware';
+import createCategoryMiddleware from '../middleware/categories/create-category-middleware'
 
 const router: Router = express.Router();
 
-router.post("/create", allowedLevels(["superadmin", "admin"]), createCategory)
+router.post("/create", allowedLevels(["superadmin", "admin"]), createCategoryMiddleware, createCategory)
 
 export default router;
