0866bc746ec1ae06a583e42a8e842ba26f9604fa
Fannn03 <fann.fann771@gmail.com>
2023-11-14 17:28:38 +0700

    test(unit test): user login service

---
+++ test/units/services/user/login-service.test.ts
@@ -0,0 +1,75 @@
+import { faker } from '@faker-js/faker'
+import { PrismaClient, User } from '@prisma/client'
+import bcrypt from 'bcrypt'
+import login, { LoginError } from '../../../../src/services/user/login-service'
+import { ulid } from 'ulid'
+
+const prisma = new PrismaClient()
+
+describe("User Login Service", () => {
+  let user: User
+
+  beforeAll(async () => {
+
+    user = await prisma.user.create({
+      data: {
+        id: ulid(),
+        email: faker.internet.email(),
+        name: faker.internet.userName(),
+        password: await bcrypt.hash('password', 10)
+      }
+    })
+  })
+
+  test("Should return null when email is not found", async () => {
+    const payload = {
+      email: "randomEmail",
+      password: "password"
+    }
+
+    const response = await login(payload)
+    expect(response).toBeNull()
+  })
+
+  test("Should return null when password value is sting null", async () => {
+    const payload = {
+      email: user.email,
+      password: ""
+    }
+
+    const response = await login(payload)
+    expect(response).toBeNull()
+  })
+
+  test("Should return LoginError class when email is not verified", async () => {
+    const payload = {
+      email: user.email,
+      password: "password"
+    }
+
+    const response = await login(payload)
+    expect(response).toBeUndefined()
+  })
+
+  test("Should return property response when login success", async () => {
+    // pretend that this session user email already activated
+    user = await prisma.user.update({
+      data: {
+        email_verified: new Date().toISOString()
+      },
+      where: {
+        email: user.email
+      }
+    })
+
+    const payload = {
+      email: user.email,
+      password: "password"
+    }
+
+    const response = await login(payload)
+    expect(response).toHaveProperty("id")
+    expect(response).toHaveProperty("name")
+    expect(response).toHaveProperty("token")
+  })
+})
