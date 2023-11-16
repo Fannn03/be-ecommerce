d2a660d7e85e20e3e61a886a297327be58ed7fe4
Fannn03 <fann.fann771@gmail.com>
2023-11-16 09:53:37 +0700

    feat(product): create product middleware

---
+++ src/middleware/products/create-product-middleware.ts
@@ -0,0 +1,49 @@
+import { NextFunction, Request, Response } from "express";
+import Joi from "joi";
+
+interface ErrorMessage {
+  name?: string
+}
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  const form = Joi.object({
+    name: Joi.string()
+      .required()
+      .trim()
+      .min(4)
+      .max(50)
+      ,
+    description: Joi.string()
+      .trim()
+      ,
+    price: Joi.number()
+      .required()
+      .min(500)
+      .max(10000000)
+      ,
+    stock: Joi.number()
+      .required()
+      .min(1)
+      .max(10000)
+  })
+
+  try {
+    await form.validateAsync(req.body, {
+      abortEarly: false
+    })
+  } catch (err) {
+    const errMessage: ErrorMessage = {}
+    
+    if(err instanceof Joi.ValidationError) {
+      err.details.map((data: Joi.ValidationErrorItem) => {
+        errMessage[data.context?.key as keyof ErrorMessage] = data.message
+      })
+    }
+
+    return res.status(400).json({
+      code: 400,
+      result: 'bad request',
+      message: errMessage
+    })
+  }
+}
--- src/routers/product-router.ts
+++ src/routers/product-router.ts
@@ -1,9 +1,10 @@
 import express, { Router } from 'express'
 import authMiddleware from '../middleware/auth-middleware'
 import { createProduct } from '../controllers/product-controller'
+import createProductMiddleware from '../middleware/products/create-product-middleware'
 
 const router: Router = express.Router()
 
-router.post('/create', authMiddleware, createProduct)
+router.post('/create', authMiddleware, createProductMiddleware, createProduct)
 
 export default router
