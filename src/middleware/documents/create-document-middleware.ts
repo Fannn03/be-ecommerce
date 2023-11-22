523a303f17e6472590781ef9753dcebd008c1176
Fannn03 <fann.fann771@gmail.com>
2023-11-22 20:06:04 +0700

    feat(document): create document middleware

---
+++ src/middleware/documents/create-document-middleware.ts
@@ -0,0 +1,66 @@
+import { NextFunction, Request, Response } from "express";
+import Joi from "joi";
+import fs from 'fs'
+
+interface ErrorMessage {
+  username?: string,
+  name?: string,
+  file?: string
+}
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  if(req.file) req.body.photos = req.file;
+
+  const form = Joi.object({
+    fullname: Joi.string()
+      .required()
+      .trim()
+      .min(4)
+      ,
+    nik: Joi.string()
+      .required()
+      .trim()
+      // source regex nik ktp https://www.huzefril.com/posts/regex/regex-ktp/
+      .regex(RegExp("^\\d{16}$"))
+      .messages({
+        "string.pattern.base": "Invalid nik value"
+      })
+      ,
+    photos: Joi.any()
+      .required()
+      .custom((value, helper) => {
+        const allowedExtension = ['image/png', 'image/jpeg', 'image/jpg'];
+
+        if(!allowedExtension.includes(value.mimetype)) {
+          return helper.error('any.invalid');
+        }
+      })
+      .messages({
+        'any.invalid': "File type must be PNG, JPG, or JPEG"
+      })
+  })
+
+  try {
+    await form.validateAsync(req.body, {
+      abortEarly: false
+    })
+  } catch (err) {
+    let errMessages: ErrorMessage = {}
+
+    if(err instanceof Joi.ValidationError) {
+      err.details.map((data: Joi.ValidationErrorItem) => {
+        if(data.context?.key) errMessages[data.context?.key as keyof ErrorMessage] = data.message;
+      })
+    }
+    // delete file if exist
+    if(req.file) fs.rmSync(req.file.path);
+
+    res.status(400).json({
+      code: 400,
+      result: 'bad request',
+      message: errMessages
+    });
+  }
+
+  return next();
+}
