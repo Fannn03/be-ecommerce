05999730e839eb5c38ae42801ed9ebcf9b0002ca
Fannn03 <fann.fann771@gmail.com>
2023-11-23 17:59:17 +0700

    feat: create document user

--- src/controllers/document-controller.ts
+++ src/controllers/document-controller.ts
@@ -1,7 +1,29 @@
 import { Request, Response } from "express";
+import createDocumentService, { CreateDocumentError } from '../services/document/create-service';
 
 export const createDocument = async (req: Request, res: Response) => {
+  try {
+    const document = await createDocumentService(req.user, req.body);
 
-  return res.send("document");
-
+    res.json({
+      code: 200,
+      result: 'success',
+      message: 'record has been created',
+      data: document
+    })
+  } catch (err: any) {
+    if (err instanceof CreateDocumentError) {
+      return res.status(err.code).json({
+        code: err.code,
+        result: err.result,
+        message: err.message
+      })
+    } else {
+      return res.status(500).json({
+        code: 500,
+        result: 'internal server error',
+        message: err.message
+      })
+    }
+  }
 }
---
+++ src/services/document/create-service.ts
@@ -0,0 +1,58 @@
+import { JwtPayload } from "jsonwebtoken";
+import fs from 'fs';
+import { UserJWT } from "../../middleware/auth-middleware";
+import { insertDocument } from "../../repositories/document"
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+
+interface documentInterface {
+  fullname  :   string,
+  nik       :   string,
+  photos    :   Express.Multer.File
+}
+
+export class CreateDocumentError {
+  constructor(public message: string, public code: number, public result: string) {
+    this.message = message;
+    this.code = code;
+    this.result = result;
+  }
+}
+
+export default async (user: UserJWT | JwtPayload, body: documentInterface) => {
+  const extension = body.photos.mimetype.split('/')[1]
+  const fileName = `${user.name}.${extension}`
+
+  try {
+    const document = await insertDocument({
+      user_id: user.id,
+      fullname: body.fullname,
+      nik: body.nik,
+      photos: fileName
+    })
+
+    fs.renameSync(body.photos.path, `public/images/documents/${document.photos}`);
+    const response = {
+      user_id: document.user_id,
+      fullname: document.fullname,
+      nik: document.nik,
+      photos: `documents/${document.photos}`,
+      createdAt: document.createdAt
+    }
+
+    return response;
+  } catch (err: any) {
+    fs.rmSync(body.photos.path);
+    
+    if(err instanceof PrismaClientKnownRequestError) {
+      if(err.code === "P2002" && err.meta?.target === "documents_user_id_key") {
+        throw new CreateDocumentError("User document already exist", 400, "bad request");
+      } else if(err.code === "P2002" && err.meta?.target === "documents_nik_key") {
+        throw new CreateDocumentError("User nik already taken", 400, "bad request");
+      } else {
+        throw err
+      }
+    } else {
+      throw new Error(err)
+    }
+  }
+}
