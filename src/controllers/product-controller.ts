338ed023628f8d46fa09ab8c764b8ed73544f7b5
Fannn03 <fann.fann771@gmail.com>
2023-11-25 18:58:10 +0700

    feat(product): add logger response

--- src/controllers/product-controller.ts
+++ src/controllers/product-controller.ts
@@ -2,30 +2,47 @@ import { Request, Response } from "express";
 import findAllProductService from '../services/product/findall-service'
 import createProductService, { CreateProductError } from '../services/product/create-service'
 import detailProductService from '../services/product/detail-service'
+import loggerResponse from "../helpers/server/logger-response";
 
 export const findAllProduct = async (req: Request, res: Response) => {
   try {
     const products = await findAllProductService(req.query)
 
     if(!products.length && req.query.page) {
-      return res.status(404).json({
+      res.status(404).json({
         code: 404,
         result: 'not found',
         message: 'record data not found'
       })
+
+      return loggerResponse({
+        req: req,
+        res: res
+      })
     }
 
-    return res.json({
+    res.json({
       code: 200,
       result: 'success',
       message: 'success get record data',
       data: products
     })
+
+    return loggerResponse({
+      req: req,
+      res: res
+    })
   } catch (err: any) {
-    return res.status(500).json({
+    res.status(500).json({
       code: 500,
       result: 'bad request',
       message: err.message
+    })
+
+    return loggerResponse({
+      req: req,
+      res: res,
+      error_message: err.message
     })
   }
 }
@@ -34,23 +51,41 @@ export const detailProduct = async (req: Request, res: Response) => {
   try {
     const product = await detailProductService(req.params.name)
 
-    if(!product) return res.status(404).json({
-      code: 404,
-      result: 'not found',
-      message: 'record not found'
-    })
+    if(!product) {
+      res.status(404).json({
+        code: 404,
+        result: 'not found',
+        message: 'record not found'
+      })
 
-    return res.json({
+      return loggerResponse({
+        req: req,
+        res: res,
+      })
+    }
+
+    res.json({
       code: 200,
       result: 'success',
       message: 'success get record data',
       data: product
     })
+
+    return loggerResponse({
+      req: req,
+      res: res,
+    })
   } catch (err: any) {
-    return res.status(500).json({
+    res.status(500).json({
       code: 500,
       result: 'internal server error',
       message: err.message
+    })
+
+    return loggerResponse({
+      req: req,
+      res: res,
+      error_message: err.message
     })
   }
 }
@@ -59,24 +94,41 @@ export const createProduct = async (req: Request, res: Response) => {
   try {
     const product = await createProductService(req)
 
-    return res.json({
+    res.json({
       code: 200,
       result: 'success',
       message: 'record has been created',
       data: product
     })
+
+    return loggerResponse({
+      req: req,
+      res: res,
+    })
   } catch (err: any) {
     if(err instanceof CreateProductError) {
-      return res.status(404).json({
+      res.status(404).json({
         code: err.code,
         result: err.result,
         message: err.message
       })
+
+      return loggerResponse({
+        req: req,
+        res: res,
+        error_message: err.message
+      })
     } else {
-      return res.status(500).json({
+      res.status(500).json({
         code: 500,
         result: 'internal server error',
         message: err.message
+      })
+
+      return loggerResponse({
+        req: req,
+        res: res,
+        error_message: err.message
       })
     }
   }
