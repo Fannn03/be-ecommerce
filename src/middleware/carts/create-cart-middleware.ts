03fbd64e83a62caeacfe69df34ed1d77a3204c8f
Fannn03 <fann.fann771@gmail.com>
2023-11-28 18:06:21 +0700

    feat: add cart middleware

---
+++ src/middleware/carts/create-cart-middleware.ts
@@ -0,0 +1,47 @@
+import { NextFunction, Request, Response } from "express";
+import Joi from "joi";
+import loggerResponse from "../../helpers/server/logger-response";
+
+interface ErrorMessage {
+  name?: string
+}
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  const form = Joi.object({
+    product_id: Joi.number()
+      .required()
+      .empty()
+      ,
+    quantity: Joi.number()
+      .required()
+      .empty()
+      .min(1)
+  })
+
+  try {
+    await form.validateAsync(req.body, {
+      abortEarly: false
+    })
+  } catch (err) {
+    const errMessage: ErrorMessage = {}
+    
+    if(err instanceof Joi.ValidationError) {
+      err.details.map((data: Joi.ValidationErrorItem) => {
+        errMessage[data.context?.key as keyof ErrorMessage] = data.message
+      })
+
+      res.status(400).json({
+        code: 400,
+        result: 'bad request',
+        message: errMessage
+      })
+
+      return loggerResponse({
+        req: req,
+        res: res
+      })
+    }
+  }
+
+  return next();
+}
