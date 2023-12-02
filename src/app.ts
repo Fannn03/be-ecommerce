ce8ef0fb01a67ba45a7c2939a163c57990595fbb
Fannn03 <fann.fann771@gmail.com>
2023-12-02 16:00:56 +0700

    chore: install cors and other dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 import 'dotenv/config'
 import apiRouter from './routers/api-router'
 
 const app: express.Application = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public/images'))
